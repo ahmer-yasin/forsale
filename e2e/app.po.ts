b5fce95d68722f80c63b281b2320a8c9ed7bb26d
ahmer yasin <ahmeryasin1992@gmail.com>
2017-07-27 21:25:49 +0500

    initial project

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class Angular4SearchDevsPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    // return element(by.css('app-root h1')).getText();
   }
 }
